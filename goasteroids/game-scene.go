b30d4a5840bf6b61ea9b6e26f7b150643a088ace
faheem047 <mdfahim.aka993@gmail.com>
2025-09-12 00:32:17 +0530

    on game screen

--- goasteroids/game-scene.go
+++ goasteroids/game-scene.go
@@ -1,24 +1,73 @@
 package goasteroids
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	baseMeteorVelocity  = 0.25
+	meteorSpawnTime     = 100 * time.Millisecond
+	meteorSpeedUpAmount = 0.1
+	meteorSpeedUpTime   = 10 * time.Second
+)
 
 type GameScene struct {
-	player *Player
+	player           *Player
+	baseVelocity     float64
+	meteorCount      int
+	meteorSpawnTimer *Timer
+	meteors          map[int]*Meteor
+	meteorsForLevel  int
+	velocityTimer    *Timer
 }
 
 func NewGameScene() *GameScene {
-	g := &GameScene{}
+	g := &GameScene{
+		meteorSpawnTimer: NewTimer(meteorSpawnTime),
+		baseVelocity:     baseMeteorVelocity,
+		velocityTimer:    NewTimer(meteorSpeedUpTime),
+		meteors:          make(map[int]*Meteor),
+		meteorsForLevel:  2,
+		meteorCount:      0,
+	}
 	g.player = NewPlayer(g)
 	return g
 }
 
 func (g *GameScene) Update(state *State) error {
 	g.player.Update()
+	g.spawnMeteors()
+	g.speedUpMeteors()
 	return nil
 }
 func (g *GameScene) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
+	//draw the meters
+	for _, m := range g.meteors {
+		m.Draw(screen)
+	}
 }
 func (g *GameScene) Layout(outsideWidth, outsideHeight int) (ScreenWidth, ScreenHeight int) {
 	return outsideWidth, outsideHeight
+}
+func (g *GameScene) spawnMeteors() {
+	g.meteorSpawnTimer.Update()
+	if g.meteorSpawnTimer.IsReady() {
+		g.meteorSpawnTimer.Reset()
+		if len(g.meteors) < g.meteorsForLevel && g.meteorCount < g.meteorsForLevel {
+			m := NewMeteor(g.baseVelocity, g, len(g.meteors)-1)
+			g.meteorCount++
+			g.meteors[g.meteorCount] = m
+		}
+	}
+}
+
+func (g *GameScene) speedUpMeteors() {
+	g.velocityTimer.Update()
+	if g.velocityTimer.IsReady() {
+		g.velocityTimer.Reset()
+		g.baseVelocity += meteorSpeedUpAmount
+	}
 }
