ef54b844b9e6f799805e58055e20e1f3843e088b
faheem047 <mdfahim.aka993@gmail.com>
2025-09-20 19:31:47 +0530

    Exploding Meteors with Laser

--- goasteroids/game-scene.go
+++ goasteroids/game-scene.go
@@ -2,6 +2,8 @@ package goasteroids
 
 import (
 	"fmt"
+	"go-asteroids/assets"
+	"math/rand"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -9,42 +11,53 @@ import (
 )
 
 const (
-	baseMeteorVelocity  = 0.25                    // The base speed for meteors.
-	meteorSpawnTime     = 100 * time.Millisecond  // How long before meteors spawn.
-	meteorSpeedUpAmount = 0.1                     // How much do we speed a meteor up when it's timer runs out.
-	meteorSpeedUpTime   = 1000 * time.Millisecond // How long to wait to speed up meteors.
+	baseMeteorVelocity   = 0.25                    // The base speed for meteors.
+	meteorSpawnTime      = 100 * time.Millisecond  // How long before meteors spawn.
+	meteorSpeedUpAmount  = 0.1                     // How much do we speed a meteor up when it's timer runs out.
+	meteorSpeedUpTime    = 1000 * time.Millisecond // How long to wait to speed up meteors.
+	cleanUpExplosionTime = 200 * time.Millisecond
 )
 
 // GameScene is the overall type for a game scene (e.g. TitleScene, GameScene, etc.).
 type GameScene struct {
-	player           *Player         // The player.
-	baseVelocity     float64         // The base velocity for items in the game.
-	meteorCount      int             // The counter for meteors.
-	meteorSpawnTimer *Timer          // The timer for spawning meteors.
-	meteors          map[int]*Meteor // A map of meteors.
-	meteorsForLevel  int             // # of meteors for a level.
-	velocityTimer    *Timer          // The timer used for speeding up meteors.
-	space            *resolv.Space   // The space for all collision objects.
-	lasers           map[int]*Laser
-	laserCount       int
+	player               *Player         // The player.
+	baseVelocity         float64         // The base velocity for items in the game.
+	meteorCount          int             // The counter for meteors.
+	meteorSpawnTimer     *Timer          // The timer for spawning meteors.
+	meteors              map[int]*Meteor // A map of meteors.
+	meteorsForLevel      int             // # of meteors for a level.
+	velocityTimer        *Timer          // The timer used for speeding up meteors.
+	space                *resolv.Space   // The space for all collision objects.
+	lasers               map[int]*Laser
+	laserCount           int
+	score                int
+	explosionSmallSprite *ebiten.Image
+	explosionSprite      *ebiten.Image
+	explosionFrames      []*ebiten.Image
+	cleanUpTimer         *Timer
 }
 
 // NewGameScene is a factory method for producing a new game. It's called once,
 // when game play starts (and again when game play restarts).
 func NewGameScene() *GameScene {
 	g := &GameScene{
-		meteorSpawnTimer: NewTimer(meteorSpawnTime),
-		baseVelocity:     baseMeteorVelocity,
-		velocityTimer:    NewTimer(meteorSpeedUpTime),
-		meteors:          make(map[int]*Meteor),
-		meteorCount:      0,
-		meteorsForLevel:  2,
-		space:            resolv.NewSpace(ScreenWidth, ScreenHeight, 16, 16),
-		lasers:           make(map[int]*Laser),
-		laserCount:       0,
+		meteorSpawnTimer:     NewTimer(meteorSpawnTime),
+		baseVelocity:         baseMeteorVelocity,
+		velocityTimer:        NewTimer(meteorSpeedUpTime),
+		meteors:              make(map[int]*Meteor),
+		meteorCount:          0,
+		meteorsForLevel:      2,
+		space:                resolv.NewSpace(ScreenWidth, ScreenHeight, 16, 16),
+		lasers:               make(map[int]*Laser),
+		laserCount:           0,
+		explosionSprite:      assets.ExplosionSprite,
+		explosionSmallSprite: assets.ExplosionSmallSprite,
+		cleanUpTimer:         NewTimer(cleanUpExplosionTime),
 	}
 	g.player = NewPlayer(g)
 	g.space.Add(g.player.playerObj)
+
+	g.explosionFrames = assets.Explosion
 
 	return g
 }
@@ -67,6 +80,8 @@ func (g *GameScene) Update(state *State) error {
 
 	g.isPlayerCollidingWithMeteor()
 
+	g.isMeteorHitByPlayerLaser()
+
 	return nil
 }
 
@@ -87,6 +102,37 @@ func (g *GameScene) Draw(screen *ebiten.Image) {
 // Layout is necessary to satisfy interface requirements from ebiten.
 func (g *GameScene) Layout(outsideWidth, outsideHeight int) (ScreeWidth, ScreenHeight int) {
 	return outsideWidth, outsideHeight
+}
+
+func (g *GameScene) isMeteorHitByPlayerLaser() {
+	for _, m := range g.meteors {
+		for _, l := range g.lasers {
+			if m.meteorObj.IsIntersecting(l.laserObj) {
+				if m.meteorObj.Tags().Has(TagSmall) {
+					// Small meteor
+					m.sprite = g.explosionSmallSprite
+					g.score++
+				} else {
+					// Large meteor
+					oldPos := m.position
+
+					m.sprite = g.explosionSprite
+
+					g.score++
+
+					numToSpawn := rand.Intn(numberOfSmallMeteorsFromLargeMeteor)
+					for i := 0; i < numToSpawn; i++ {
+						meteor := NewSmallMeteor(baseMeteorVelocity, g, len(m.game.meteors)-1)
+						meteor.position = Vector{oldPos.X + float64(rand.Intn(100-50)+50), oldPos.Y + float64(rand.Intn(100-50)+50)}
+						meteor.meteorObj.SetPosition(meteor.position.X, meteor.position.Y)
+						g.space.Add(meteor.meteorObj)
+						g.meteorCount++
+						g.meteors[m.game.meteorCount] = meteor
+					}
+				}
+			}
+		}
+	}
 }
 
 // spawnMeteors creates meteors, up to the maximum for a level.
--- goasteroids/meteor.go
+++ goasteroids/meteor.go
@@ -15,91 +15,155 @@ const (
 	numberOfSmallMeteorsFromLargeMeteor = 4
 )
 
+// Meteor is the type for all meteors, both big and small.
 type Meteor struct {
-	game          *GameScene
-	position      Vector
-	rotation      float64
-	movement      Vector
-	angle         float64
-	rotationSpeed float64
-	sprite        *ebiten.Image
-	meteorObj     *resolv.Circle
+	game          *GameScene     // Embed the game so we have access to it.
+	position      Vector         // Where is the meteor.
+	rotation      float64        // The rotation for the meteor.
+	movement      Vector         // What direction is it going.
+	angle         float64        // What angle is it moving at.
+	rotationSpeed float64        // The speed of rotation.
+	sprite        *ebiten.Image  // The image.
+	meteorObj     *resolv.Circle // The collision object.
 }
 
+// NewMeteor is a factory method which creates a new large meteor.
 func NewMeteor(baseVelocity float64, g *GameScene, index int) *Meteor {
-	// Target the centre of the screen
-
+	// Target the center of the screen.
 	target := Vector{
 		X: ScreenWidth / 2,
 		Y: ScreenHeight / 2,
 	}
 
-	// Pick a random angle
-
+	// Pick a random angle. 2π is 360°, so this returns an angle between 0° to 360°.
 	angle := rand.Float64() * 2 * math.Pi
 
-	// The Distance from the centre that meteor should spawn at . Half the width , add some arbitrary distance
+	// The distance from the center that meteor should spawn at. Half the width, add some arbitrary distance.
 	r := ScreenWidth/2.0 + 500
 
-	// Create the Position Vector , using the angle and simple math
-
+	// Create the position vector, using the angle and simple math.
 	pos := Vector{
 		X: target.X + math.Cos(angle)*r,
 		Y: target.Y + math.Sin(angle)*r,
 	}
 
-	// Keep the meteor moving towards the centre of the screen
-	// Give it a random velocity
-
+	// Keep the meteor moving towards the center of the screen.
+	// Give it a random velocity.
 	velocity := baseVelocity + rand.Float64()*1.5
 
-	// Create the direction vector and normalize it
+	// Create the direction vector and normalize it.
 	direction := Vector{
 		X: target.X - pos.X,
 		Y: target.Y - pos.Y,
 	}
 	normalizedDirection := direction.Normalize()
 
-	// Create the movement Vector.
+	// Create the movement vector.
 	movement := Vector{
 		X: normalizedDirection.X * velocity,
 		Y: normalizedDirection.Y * velocity,
 	}
-	// Assign a sprite to the meteor
+
+	// Assign a sprite to the meteor.
 	sprite := assets.MeteorSprites[rand.Intn(len(assets.MeteorSprites))]
 
-	// create collison obj
-	meteorObj := resolv.NewCircle(pos.X, pos.Y, float64(sprite.Bounds().Dx())/2)
+	// Create the collision object.
+	meteorObj := resolv.NewCircle(pos.X, pos.Y, float64(sprite.Bounds().Dx()/2))
 
-	// Create a meteor object and return it
+	// Create a meteor object and return it.
 	m := &Meteor{
 		game:          g,
 		position:      pos,
-		rotation:      angle,
 		movement:      movement,
-		angle:         angle,
-		rotationSpeed: rand.Float64()*(rotationSpeedMax-rotationSpeedMin) + rotationSpeedMin,
+		rotationSpeed: rotationSpeedMin + rand.Float64()*(rotationSpeedMax-rotationSpeedMin),
 		sprite:        sprite,
+		angle:         angle,
 		meteorObj:     meteorObj,
 	}
-	m.meteorObj.Tags().Set(TagMeteor | TagLarge)
+
 	m.meteorObj.SetPosition(pos.X, pos.Y)
-	m.meteorObj.SetData(&ObjectData{
-		index: index,
-	})
+	m.meteorObj.Tags().Set(TagMeteor | TagLarge)
+	m.meteorObj.SetData(&ObjectData{index: index})
+
 	return m
 }
+
+// NewSmallMeteor is a factory method which creates a new small meteor.
+func NewSmallMeteor(baseVelocity float64, g *GameScene, index int) *Meteor {
+	// Target the center of the screen.
+	target := Vector{
+		X: ScreenWidth / 2,
+		Y: ScreenHeight / 2,
+	}
+
+	// Pick a random angle. 2π is 360°, so this returns an angle between 0° to 360°.
+	angle := rand.Float64() * 2 * math.Pi
+
+	// The distance from the center that meteor should spawn at. Half the width, add some arbitrary distance.
+	r := ScreenWidth/2.0 + 500
+
+	// Create the position vector, using the angle and simple math.
+	pos := Vector{
+		X: target.X + math.Cos(angle)*r,
+		Y: target.Y + math.Sin(angle)*r,
+	}
+
+	// Keep the meteor moving towards the center of the screen.
+	// Give it a random velocity.
+	velocity := baseVelocity + rand.Float64()*1.5
+
+	// Create the direction vector and normalize it.
+	direction := Vector{
+		X: target.X - pos.X,
+		Y: target.Y - pos.Y,
+	}
+	normalizedDirection := direction.Normalize()
+
+	// Create the movement vector.
+	movement := Vector{
+		X: normalizedDirection.X * velocity,
+		Y: normalizedDirection.Y * velocity,
+	}
+
+	// Assign a sprite to the meteor.
+	sprite := assets.MeteorSpritesSmall[rand.Intn(len(assets.MeteorSpritesSmall))]
+
+	// Create the collision object.
+	meteorObj := resolv.NewCircle(pos.X, pos.Y, float64(sprite.Bounds().Dx()/2))
+
+	// Create a meteor object and return it.
+	m := &Meteor{
+		game:          g,
+		position:      pos,
+		movement:      movement,
+		rotationSpeed: rotationSpeedMin + rand.Float64()*(rotationSpeedMax-rotationSpeedMin),
+		sprite:        sprite,
+		angle:         angle,
+		meteorObj:     meteorObj,
+	}
+
+	m.meteorObj.SetPosition(pos.X, pos.Y)
+	m.meteorObj.Tags().Set(TagMeteor | TagSmall)
+	m.meteorObj.SetData(&ObjectData{index: index})
+
+	return m
+}
+
+// Update updates all game scene elements for the next draw. It's called once per tick.
 func (m *Meteor) Update() {
 	dx := m.movement.X
 	dy := m.movement.Y
+
 	m.position.X += dx
 	m.position.Y += dy
 	m.rotation += m.rotationSpeed
 
-	// keep the meteor on the screen
+	// Keep meteor on screen.
 	m.keepOnScreen()
 	m.meteorObj.SetPosition(m.position.X, m.position.Y)
 }
+
+// Draw draws all game elements to the screen. It's called once per frame.
 func (m *Meteor) Draw(screen *ebiten.Image) {
 	bounds := m.sprite.Bounds()
 	halfW := float64(bounds.Dx()) / 2
@@ -109,10 +173,13 @@ func (m *Meteor) Draw(screen *ebiten.Image) {
 	op.GeoM.Translate(-halfW, -halfH)
 	op.GeoM.Rotate(m.rotation)
 	op.GeoM.Translate(halfW, halfH)
+
 	op.GeoM.Translate(m.position.X, m.position.Y)
+
 	screen.DrawImage(m.sprite, op)
 }
 
+// keepOnScreen keeps meteors on the screen.
 func (m *Meteor) keepOnScreen() {
 	if m.position.X >= float64(ScreenWidth) {
 		m.position.X = 0
